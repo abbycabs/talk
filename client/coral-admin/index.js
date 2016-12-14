a9a972b9266401233da03a078c3c36d7c6faf38e
Belen Curcio <curciobelen@gmail.com>
2016-12-14 08:44:44 -0300

    Adding index.js to the modules

--- client/coral-admin/index.js
+++ client/coral-admin/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './src/components/App';
 
 // Render the application into the DOM
 ReactDOM.render(<App />, document.querySelector('#root'));
--- client/coral-embed-stream/index.js
+++ client/coral-embed-stream/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
-import CommentStream from './CommentStream';
+import CommentStream from './src/CommentStream';
 import {Provider} from 'react-redux';
-import {store} from '../../coral-framework';
+import {store} from '../coral-framework';
 
 render(
     <Provider store={store}>
